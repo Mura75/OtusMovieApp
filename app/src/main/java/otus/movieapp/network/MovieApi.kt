458f5566704b4e98d410d1b5463a1ac6a2ef6c20
murager <murager@codebusters.team>
2020-04-07 18:22:15 +0600

    Movies responses and java 8 dependency added.

--- app/src/main/java/otus/movieapp/network/MovieApi.kt
+++ app/src/main/java/otus/movieapp/network/MovieApi.kt
@@ -2,13 +2,18 @@ package otus.movieapp.network
 
 import kotlinx.coroutines.flow.Flow
 import otus.movieapp.model.Movie
+import otus.movieapp.model.MoviesResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MovieApi {
 
     @GET("movie/{movie_id}")
     suspend fun getMovie(@Path("movie_id") movieId: Int) : Flow<Response<Movie>>
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("page") page: Int) : Response<MoviesResponse>
 
 }
